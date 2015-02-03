96a048e659ede7bc361cc6bad66441ab35c3ec54
Paul McAdam <prmcadam@gmail.com>
2015-02-03 11:37:40 +1100

    tweak results parsing

--- findFlanks.py
+++ findFlanks.py
@@ -3,22 +3,34 @@ from Bio.Blast import NCBIXML
 from Bio import SeqIO
 import sys
 
+
+
 def runBlastn(q_seq, s_seq, out_file):
 	blastn_cline = NcbiblastnCommandline(query = q_seq, subject = s_seq, evalue=0.001, outfmt=5, out = out_file)
 	stdout,stderr=blastn_cline()
 	return
 	
-def parseBlastResults(result_handle):
-	with open(result_handle,'r') as f:
-		blast_records = NCBIXML.parse(f)
-		for alignment in blast_record.alignments:
-			print alignment
-			for hsp in alignment.hsps:
-				print hsp
+def parseBlastResults(blast_record):
+#	with open(result_handle,'r') as f:
+#		blast_records = NCBIXML.parse(f)
+	print blast_record
+	for alignment in blast_record.alignments:
+		print alignment
 	return
 	
 	
-runBlastn(sys.argv[1], sys.argv[2], sys.argv[3])
 
 
-parseBlastResults(sys.argv[3])
+
+
+
+###########################
+
+if __name__ == '__main__':
+	runBlastn(sys.argv[1], sys.argv[2], sys.argv[3])
+	with open(sys.argv[3], 'r') as f:
+		blast_records = NCBIXML.parse(f)
+		for blast_record in blast_records:
+		parseBlastResults(blast_record)
+	
+	
