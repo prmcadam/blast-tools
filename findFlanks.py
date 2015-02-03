18f6899d76441e55b442a19176f5bd3593bd4fdf
Paul McAdam (University of Melbourne) <pmcadam@barcoo.pcf.vlsci.unimelb.edu.au>
2015-02-03 11:26:28 +1100

    figuring out blastn cline

--- findFlanks.py
+++ findFlanks.py
@@ -4,18 +4,21 @@ from Bio import SeqIO
 import sys
 
 def runBlastn(q_seq, s_seq, out_file):
-	blastn_cline = NcbiblastnCommandline(query = q_seq, db = s_seq, evalue=0.001, outfmt=5, out = out_file)
+	blastn_cline = NcbiblastnCommandline(query = q_seq, subject = s_seq, evalue=0.001, outfmt=5, out = out_file)
 	stdout,stderr=blastn_cline()
 	return
 	
 def parseBlastResults(result_handle):
-	blast_record = NCBIXML.read(result_handle)
-	for alignment in blast_record.alignments:
-		for hsp in alignment.hsps:
-			print hsp
+	with open(result_handle,'r') as f:
+		blast_records = NCBIXML.parse(f)
+		for alignment in blast_record.alignments:
+			print alignment
+			for hsp in alignment.hsps:
+				print hsp
 	return
 	
 	
 runBlastn(sys.argv[1], sys.argv[2], sys.argv[3])
+
 
 parseBlastResults(sys.argv[3])
